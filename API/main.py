bb82ac99fdbad95566d2bcb196b80284eaa9d9bc
Mohamed Hany <mhany4485@gmail.com>
2024-12-05 01:54:19 +0200

    re-implemented better file-saving

--- API/main.py
+++ API/main.py
@@ -3,12 +3,25 @@ from parseData import *
 import json
 import os
 
-f = open("dataLog.json", "w")
-big_data = []
-for data in fetch_data(country="", limit=100):
+f = open("./dataLog.json", "w")
+f.write("[")
+def dynamic_write(data):
+    f.write(json.dumps(data))
+    f.write(", ")
 
-    big_data.append(parse_data(data.json()["results"]))
+
+total_count = {"total_count": 0}
+
+for data in fetch_data(country="", limit=10, pages=2, type="Provoked"):
+    print(data.json())
+    #big_data.append(parse_data(data.json()["results"]))
+    parsed_data = parse_data(data.json()["results"])
+    dynamic_write(parsed_data)
+    total_count["total_count"]+=len(parsed_data)
     print("aa") 
     
-(json.dump(big_data, f))
+json.dump(total_count, f)
+
+
+f.write("]")
 f.close()
