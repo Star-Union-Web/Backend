66bbdfa2ba5ea643c581b9645db499d453ce9a7f
MahmoudHesham1350 <mahmoudhesham656@gmail.com>
2024-12-10 22:49:38 +0200

    handing in commit

--- Personal Blog (task5)/env/PersonalBlog/Posts/forms.py
+++ Personal Blog (task5)/env/PersonalBlog/Posts/forms.py
@@ -4,9 +4,10 @@ from .models import Post, Comment
 class PostForm(ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'content', 'is_published']
+        fields = ['title', 'content', 'slug', 'is_published']
         labels = {
             'title': 'Title',
             'content': 'Content',
+            'slug': 'URL',
             'is_published': 'Publish'  
         }
